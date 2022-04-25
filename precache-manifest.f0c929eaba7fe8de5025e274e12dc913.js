a641ac4b8176e08a464e9c1b35d1078af488ff2f
Jason Lavigne <jasonlavigne824@gmail.com>
2022-04-25 13:07:19 +0200

    Updates

--- precache-manifest.f0c929eaba7fe8de5025e274e12dc913.js
+++ precache-manifest.f0c929eaba7fe8de5025e274e12dc913.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "60ee0d47d0705d132b1025fbc69df633",
+    "revision": "6b17fa37efc93f63bd714b87bc0898e6",
     "url": "/Site-Vitrine/index.html"
   },
   {
-    "revision": "b196033b4ea31fe74e2e",
+    "revision": "c94d9dacb1b1973458d5",
     "url": "/Site-Vitrine/static/css/main.210eddce.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Site-Vitrine/static/js/2.b1683f98.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b196033b4ea31fe74e2e",
-    "url": "/Site-Vitrine/static/js/main.8456fa2a.chunk.js"
+    "revision": "c94d9dacb1b1973458d5",
+    "url": "/Site-Vitrine/static/js/main.89e2c7a5.chunk.js"
   },
   {
     "revision": "d91f4ab1b301f10611f5",
