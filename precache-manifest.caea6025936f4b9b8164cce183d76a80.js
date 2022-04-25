f9c5ddeaf3a7ec2089b3580a69b476c47a68c830
Jason Lavigne <jasonlavigne824@gmail.com>
2022-04-25 12:32:34 +0200

    Updates

---
+++ precache-manifest.caea6025936f4b9b8164cce183d76a80.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e713671f13b88c1e14eb215fa6c2d7c2",
+    "url": "/Site-Vitrine/index.html"
+  },
+  {
+    "revision": "313858b80136cb2a4534",
+    "url": "/Site-Vitrine/static/css/main.210eddce.chunk.css"
+  },
+  {
+    "revision": "a603c65cda10060e4303",
+    "url": "/Site-Vitrine/static/js/2.b1683f98.chunk.js"
+  },
+  {
+    "revision": "7d3493ef975d11c01bdd51804388be81",
+    "url": "/Site-Vitrine/static/js/2.b1683f98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "313858b80136cb2a4534",
+    "url": "/Site-Vitrine/static/js/main.ec4367d8.chunk.js"
+  },
+  {
+    "revision": "d91f4ab1b301f10611f5",
+    "url": "/Site-Vitrine/static/js/runtime-main.e025ab57.js"
+  },
+  {
+    "revision": "6f82017092b3f62c335565b373a29482",
+    "url": "/Site-Vitrine/static/media/KronaOne-Regular.6f820170.ttf"
+  },
+  {
+    "revision": "37c8a1aaa2c1891aa0960ff03c377649",
+    "url": "/Site-Vitrine/static/media/Questrial-Regular.37c8a1aa.ttf"
+  }
+]);
